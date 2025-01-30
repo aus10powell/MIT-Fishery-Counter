b284fb8f617238be30dbd69da22a3bdf8d444b7c
Austin <7726251+aus10powell@users.noreply.github.com>
2025-01-29 17:19:08 -0800

    chore: added try except to inference

--- code/src/inference_modules.py
+++ code/src/inference_modules.py
@@ -66,109 +66,112 @@ class InferenceCounter:
                 - List: List of relative frame times.
                 - List: List of detection counts for each frame.
         """
-        assert os.path.exists(video_path), f"Video file not found at '{video_path}'"
-        assert os.path.exists(
-            self.tracker
-        ), f"Tracker file not found at '{self.tracker}'"
-        video = cv2.VideoCapture(video_path)
+        try:
+            assert os.path.exists(video_path), f"Video file not found at '{video_path}'"
+            assert os.path.exists(
+                self.tracker
+            ), f"Tracker file not found at '{self.tracker}'"
+            video = cv2.VideoCapture(video_path)
 
-        _, frame = video.read()
+            _, frame = video.read()
 
-        frame_rate = video.get(cv2.CAP_PROP_FPS)
-        total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-        duration_seconds = total_frames / frame_rate
-        time_interval = 1.0 / frame_rate
+            frame_rate = video.get(cv2.CAP_PROP_FPS)
+            total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+            duration_seconds = total_frames / frame_rate
+            time_interval = 1.0 / frame_rate
 
-        frame_height, frame_width, _ = frame.shape
-        LINE_START = sv.Point(int(frame_width / 3), 0)
-        LINE_END = sv.Point(int(frame_width / 3), frame_height)
-        line_counter = sv.LineZone(start=LINE_START, end=LINE_END)
-        line_annotator = sv.LineZoneAnnotator(
-            thickness=2, text_thickness=1, text_scale=0.2
-        )
-        box_annotator = sv.BoxAnnotator(thickness=1, text_thickness=1, text_scale=0.3)
-
-        parameters = {
-            "video_path": video_path,
-            "tracker": self.tracker,
-            "line_start": LINE_START,
-            "line_end": LINE_END,
-            "frame_rate": frame_rate,
-            "confidence_threshold": conf_threshold,
-        }
-        self.logger.info(
-            "********** Running inference with the following parameters: **********"
-        )
-        self.logger.info(parameters)
-        self.logger.info("-" * 100)
-
-        annotated_frames = []
-        frame_detections = []
-        relative_frame_times = []
-
-        for frame_index, result in enumerate(
-            self.model.track(
-                imgsz=frame_width,
-                source=video_path,
-                device=self.device,
-                show=show,
-                stream=stream,
-                conf=conf_threshold,
-                tracker=self.tracker,
+            frame_height, frame_width, _ = frame.shape
+            LINE_START = sv.Point(int(frame_width / 3), 0)
+            LINE_END = sv.Point(int(frame_width / 3), frame_height)
+            line_counter = sv.LineZone(start=LINE_START, end=LINE_END)
+            line_annotator = sv.LineZoneAnnotator(
+                thickness=2, text_thickness=1, text_scale=0.2
             )
-        ):
-            frame = result.orig_img
+            box_annotator = sv.BoxAnnotator(thickness=1, text_thickness=1, text_scale=0.3)
 
-            current_frame_time = frame_index * time_interval
-            relative_frame_times.append(current_frame_time)
+            parameters = {
+                "video_path": video_path,
+                "tracker": self.tracker,
+                "line_start": LINE_START,
+                "line_end": LINE_END,
+                "frame_rate": frame_rate,
+                "confidence_threshold": conf_threshold,
+            }
+            self.logger.info(
+                "********** Running inference with the following parameters: **********"
+            )
+            self.logger.info(parameters)
+            self.logger.info("-" * 100)
 
-            detections = sv.Detections.from_ultralytics(result)
-            if result.boxes.id is not None:
-                detections.tracker_id = result.boxes.id.cpu().numpy().astype(int)
-            labels = [
-                f"tracker_id: {tracker_id} {self.model.model.names[class_id]} {confidence:0.2f}"
-                for idx, (bbox, _, confidence, class_id, tracker_id) in enumerate(
-                    detections
+            annotated_frames = []
+            frame_detections = []
+            relative_frame_times = []
+
+            for frame_index, result in enumerate(
+                self.model.track(
+                    imgsz=frame_width,
+                    source=video_path,
+                    device=self.device,
+                    show=show,
+                    stream=stream,
+                    conf=conf_threshold,
+                    tracker=self.tracker,
                 )
-            ]
-            if len(detections) > 0:
-                frame_detections.append(len(detections))
-            else:
-                frame_detections.append(0)
+            ):
+                frame = result.orig_img
 
-            frame = box_annotator.annotate(
-                scene=frame, detections=detections, labels=labels
+                current_frame_time = frame_index * time_interval
+                relative_frame_times.append(current_frame_time)
+
+                detections = sv.Detections.from_ultralytics(result)
+                if result.boxes.id is not None:
+                    detections.tracker_id = result.boxes.id.cpu().numpy().astype(int)
+                labels = [
+                    f"tracker_id: {tracker_id} {self.model.model.names[class_id]} {confidence:0.2f}"
+                    for idx, (bbox, _, confidence, class_id, tracker_id) in enumerate(
+                        detections
+                    )
+                ]
+                if len(detections) > 0:
+                    frame_detections.append(len(detections))
+                else:
+                    frame_detections.append(0)
+
+                frame = box_annotator.annotate(
+                    scene=frame, detections=detections, labels=labels
+                )
+
+                line_counter.trigger(detections=detections)
+
+                line_annotator.annotate(frame=frame, line_counter=line_counter)
+
+                if show:
+                    cv2.imshow("yolo:", frame)
+
+                annotated_frames.append(frame)
+
+                if cv2.waitKey(30) == 27 and show:
+                    break
+
+            self.logger.info("*" * 100)
+            self.logger.info(
+                f"TOTAL FISH OUT: {line_counter.out_count} \t TOTAL FISH IN: {line_counter.in_count} \t NET (moving right to left): {line_counter.out_count - line_counter.in_count}"
             )
+            self.logger.info(f"total frame_detections: {sum(frame_detections)}")
+            self.logger.info("*" * 100)
 
-            line_counter.trigger(detections=detections)
-
-            line_annotator.annotate(frame=frame, line_counter=line_counter)
-
-            if show:
-                cv2.imshow("yolo:", frame)
-
-            annotated_frames.append(frame)
-
-            if cv2.waitKey(30) == 27 and show:
-                break
-
-        self.logger.info("*" * 100)
-        self.logger.info(
-            f"TOTAL FISH OUT: {line_counter.out_count} \t TOTAL FISH IN: {line_counter.in_count} \t NET (moving right to left): {line_counter.out_count - line_counter.in_count}"
-        )
-        self.logger.info(f"total frame_detections: {sum(frame_detections)}")
-        self.logger.info("*" * 100)
-
-        return (
-            frame_rate,
-            annotated_frames,
-            line_counter.out_count,
-            line_counter.in_count,
-            duration_seconds,
-            relative_frame_times,
-            frame_detections,
-        )
-
+            return (
+                frame_rate,
+                annotated_frames,
+                line_counter.out_count,
+                line_counter.in_count,
+                duration_seconds,
+                relative_frame_times,
+                frame_detections,
+            )
+        except Exception as e:
+            self.logger.error(f"Error during inference: {str(e)}", exc_info=True)
+            raise
 
 if __name__ == "__main__":
     # Define the parameters
